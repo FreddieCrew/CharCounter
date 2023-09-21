1af5a958dfdc260167f4efc93cbbe15b8711ff94
John Michael Reyes <120303127+FreddieCrew@users.noreply.github.com>
2023-09-21 01:52:57 -0500

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,29 +1,14 @@
 @echo off
 
-rem Check if the build directory exists; if not, create it
-if not exist build mkdir build
+REM Output executable name
+set OUTPUT=CharCounter.exe
 
-rem Navigate to the build directory
-cd build
+REM Compilation using the default system compiler (assumed to be Visual C++ Compiler)
+cl /std:c++17 /Fe:%OUTPUT% main.cpp
 
-rem Run CMake to configure the project with Visual Studio 2022
-cmake .. -G "Visual Studio 17 2022"
-
-rem Compile the project using MSBuild in "Release" mode
-msbuild CharCounter.sln /p:Configuration=Release
-
-rem Check the return code of MSBuild to determine if the compilation was successful and display an appropriate message
-if %errorlevel% equ 0 (
-    echo Compilation successful.
+REM Check if compilation was successful
+if %ERRORLEVEL% == 0 (
+    echo Compilation successful. Run %OUTPUT% to execute the program.
 ) else (
-    echo There was an error during compilation.
+    echo Error during compilation.
 )
-
-rem Run the compiled program
-start Release\CharCounter.exe
-
-rem Return to the original directory
-cd ..
-
-rem Pause for message visibility (optional)
-pause
